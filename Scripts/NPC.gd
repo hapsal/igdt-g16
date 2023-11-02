2a02c62e11ea2c85e17b244f453faa95a084b13c
SalminenLauri <61330163+SalminenLauri@users.noreply.github.com>
2023-11-02 10:37:00 +0200

    feat: interaction for prototype

---
+++ Scripts/NPC.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+var active = false
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	body_entered.connect(_on_NPC_body_entered)
+	body_exited.connect(_on_NPC_body_exited)
+	pass # Replace with function body.
+
+func _on_NPC_body_entered(body):
+		active = true
+
+
+
+func _on_NPC_body_exited(body):
+		active = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	$Indicator.visible = active
+	if active == true && Input.is_action_just_pressed("Interact") == true:
+		print("interact")
+	pass
